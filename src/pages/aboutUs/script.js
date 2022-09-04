fbf93e91b2f4a95feec249225abd52e9756f8ab1
Savenok Marta <magnolia.sav@gmail.com>
2022-09-04 21:54:28 +0300

    add about us page

---
+++ src/pages/aboutUs/script.js
@@ -0,0 +1,4 @@
+const meridianBtn = document.querySelector('.meridian__button');
+meridianBtn.addEventListener('click' , () => {
+    window.location.href = 'http://localhost:1234/pages/collection__page/collections.html';
+})
